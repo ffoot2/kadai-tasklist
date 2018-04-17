791fd2608f378a2885d929eb6d145282028f56ee
EC2 Default User <ec2-user@ip-172-31-27-193.us-east-2.compute.internal>
2018-04-17 11:28:24 +0000

    Complete

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+
+class TasksController extends Controller
+{
+    public function index(){
+        // dd('b');
+        $tasks = Task::all();
+        // dd('a');
+        return view('tasks.index', ['tasks' => $tasks]);
+    }
+
+    public function show($id){
+      $task = Task::find($id);
+
+      return view('tasks.show', ['task' => $task]);
+    }
+
+    public function create(){
+      $task = new Task;
+
+      return view('tasks.create', ['task' => $task]);
+    }
+
+    public function store(Request $req){
+      $task = new Task;
+
+      $task->content = $req->content;
+      $task->save();
+
+      return redirect('/');
+    }
+
+    public function edit($id){
+      $task = Task::find($id);
+
+      return view('tasks.edit', ['task' => $task]);
+    }
+
+    public function update(Request $req, $id){
+      $task = Task::find($id);
+      $task->content = $req->content;
+      $task->save();
+
+      return redirect('/');
+    }
+
+    public function destroy($id){
+      $task = Task::find($id);
+      $task->delete();
+
+      return redirect('/');
+    }
+    
+}
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <h1>タスク一覧</h1>
+  @if(count($tasks) >0)
+    <ul>
+      @foreach($tasks as $task)
+        <li>{!! link_to_route('tasks.show', $task->content, ['id' => $task->id]) !!}</li>
+      @endforeach
+    </ul>
+    
+  @endif
+  {!! link_to_route('tasks.create', '新規タスクの作成') !!}
+@endsection
